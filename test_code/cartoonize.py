98e77393e2d790885bc5cf7e1cda4cb97f2e62a4
Jarno Ralli <jarno@ralli.fi>
2023-05-02 17:10:40 +0300

    cartoonize.py can now process directories or single images

--- test_code/cartoonize.py
+++ test_code/cartoonize.py
@@ -1,11 +1,24 @@
 import os
+import sys
 import cv2
 import numpy as np
 import tensorflow as tf 
-import network
-import guided_filter
 from tqdm import tqdm
 import argparse
+import network
+import guided_filter
+
+def path_is_directory(input_string):
+    if os.path.isdir(input_string):
+        return input_string
+    else:
+        raise NotADirectoryError(input_string)
+
+def path_exists(input_string):
+    if os.path.exists(input_string):
+        return input_string
+    else:
+        raise FileNotFoundError(input_string)
 
 def resize_crop(image):
     h, w, c = np.shape(image)
@@ -20,8 +33,7 @@ def resize_crop(image):
     image = image[:h, :w, :]
     return image
 
-
-def cartoonize(load_folder, save_folder, model_path, rho: float):
+def cartoonize(input_path, save_folder, model_path, rho):
     input_photo = tf.placeholder(tf.float32, [1, None, None, 3])
     network_out = network.unet_generator(input_photo)
     final_out = guided_filter.guided_filter(input_photo, network_out, r=rho, eps=5e-3)
@@ -36,36 +48,58 @@ def cartoonize(load_folder, save_folder, model_path, rho: float):
 
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, tf.train.latest_checkpoint(model_path))
-    name_list = os.listdir(load_folder)
 
-    for name in tqdm(name_list):
-        try:
-            load_path = os.path.join(load_folder, name)
-            save_path = os.path.join(save_folder, name)
-            image = cv2.imread(load_path)
-            if image is None:
-                continue
-            image_shape = image.shape
-            image = resize_crop(image)
-            batch_image = image.astype(np.float32)/127.5 - 1
-            batch_image = np.expand_dims(batch_image, axis=0)
-            output = sess.run(final_out, feed_dict={input_photo: batch_image})
-            output = (np.squeeze(output)+1)*127.5
-            output = np.clip(output, 0, 255).astype(np.uint8)
-            output = cv2.resize(output, (image_shape[1], image_shape[0]))
-            cv2.imwrite(save_path, output)
-        except:
-            print('cartoonize {} failed'.format(load_path))
+    file_list = []
+
+    # If the 'input_path' points to a directory, we traverse all the subdirectories and add all the files to be processed later on.
+    if os.path.isdir(input_path):
+        # Walk through the directory 'input_path' and append the results to a list of dictionaries
+        for abs_path, dirs, files in os.walk(input_path):
+            for file_ in files:
+                # Relative output directory
+                rel_path = os.path.relpath(abs_path, input_path)
+                # Combined output folder
+                combined_output_folder = os.path.join(save_folder, rel_path)
+                file_dict = {"input_image": os.path.join(abs_path, file_), "output_image": os.path.join(combined_output_folder, file_)}
+                file_list.append(file_dict)
+                if not os.path.exists(combined_output_folder):
+                    os.makedirs(combined_output_folder)
+    # If the 'input_path' points to a file, then we add this file to be processed later on.
+    elif os.path.isfile(input_path):
+        file_list = [{"input_image": str(input_path), "output_image": os.path.join(save_folder, os.path.basename(input_path))}]
+    else:
+        raise Exception(f"URL '{file_dir}' doesn't appear to be a directory nor a file")
+
+    # Iterate through the list of dictionaries
+    for file_dict in file_list:
+        image = cv2.imread(file_dict["input_image"])
+        if image is None:
+            continue
+
+        print(f'{file_dict["input_image"]} -> {file_dict["output_image"]}')
+        image_shape = image.shape
+        image = resize_crop(image)
+        batch_image = image.astype(np.float32)/127.5 - 1.0
+        batch_image = np.expand_dims(batch_image, axis=0)
+        output = sess.run(final_out, feed_dict={input_photo: batch_image})
+        output = (np.squeeze(output)+1)*127.5
+        output = np.clip(output, 0, 255).astype(np.uint8)
+        output = cv2.resize(output, (image_shape[1], image_shape[0]))
+        cv2.imwrite(file_dict["output_image"], output)
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-m", "--model_path", help="model path", default="saved_models")
-    argParser.add_argument("-i", "--input_folder", help="input folder", default="test_images")
-    argParser.add_argument("-o", "--output_folder", help="output folder", default="cartoonized_images")
+    argParser.add_argument("-m", "--model_path", help="model path", type=path_is_directory, default="saved_models")
+    argParser.add_argument("-i", "--input", help="path to a directory or a single image to process", type=path_exists, default="test_images")
+    argParser.add_argument("-o", "--output_folder", help="path to output folder", type=path_is_directory, default="cartoonized_images")
     argParser.add_argument("-r", "--rho", help="image sharpness", type=float, default=1.0)
     args = argParser.parse_args()
 
-    if not os.path.exists(args.output_folder):
-        os.mkdir(args.output_folder)
-    cartoonize(args.input_folder, args.output_folder, args.model_path, args.rho)
+    try:
+        cartoonize(args.input, args.output_folder, args.model_path, args.rho)
+    except Exception as error:
+        print(error)
+        sys.exit(1)
+    else:
+        sys.exit(0)
 
